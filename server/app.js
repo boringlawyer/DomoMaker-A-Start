6b73c1a99d27a54e940d28a39d5c17cf729ff3e0
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-04-01 13:58:32 -0400

    Finished Domomaker part C

--- server/app.js
+++ server/app.js
@@ -7,7 +7,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
-
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const csrf = require('csurf');
+const redis = require('redis');
 const port = process.env.PORT || process.env.NODE_ENV || 3000;
 
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
@@ -24,26 +27,56 @@ mongoose.connect(dbURL, mongooseOptions, (err) => {
   }
 });
 
+let redisURL = {
+    hostname: "redis-13370.c14.us-east-1-3.ec2.cloud.redislabs.com",
+    port: '13370'
+};
+
+let redisPASS = 'dg5NUBQSyggsioICTYwqT7hhIBK1Tsep'
+
+if (process.env.REDISCLOUD_URL) {
+    redisURL = url.parse(process.env.REDISCLOUD_URL);
+    redisPASS = redisURL.auth.split(':')[1];
+}
+let redisClient = redis.createClient({
+    host: redisURL.hostname,
+    port: redisURL.port,
+    password: redisPASS,
+});
+
 const router = require('./router');
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
 app.use(session({
   key: 'sessionId',
+  store: new RedisStore({
+      client: redisClient,
+  }),
   secret: 'Domo Arigato',
   resave: true,
   saveUninitialized: true,
+  cookie: {
+      httpOnly: true,
+  }
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
+app.use(csrf());
+app.use((err, req, res, next) => {
+    if (err.code !== 'EBADCSRFTOKEN') return next(err);
 
+    console.log('Missing CSRF token');
+    return false;
+});
 router(app);
 
 app.listen(port, (err) => {
