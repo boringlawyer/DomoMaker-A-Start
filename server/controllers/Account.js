f3a8ccede2a3a7a335f8582255f45c1117af159d
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-04-03 15:05:15 -0400

    Finished Part D

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -6,9 +6,6 @@ const loginPage = (req, res) => {
   res.render('login', { csrfToken: req.csrfToken() });
 };
 
-const signupPage = (req, res) => {
-  res.render('signup', { csrfToken: req.csrfToken() });
-};
 
 const logout = (req, res) => {
   req.session.destroy();
@@ -73,8 +70,16 @@ const signup = (req, res) => {
   });
 };
 
+const getToken = (req, res) => {
+  const csrfJSON = {
+    csrfToken: req.csrfToken(),
+  };
+
+  res.json(csrfJSON);
+};
+
 module.exports.loginPage = loginPage;
 module.exports.login = login;
 module.exports.logout = logout;
-module.exports.signupPage = signupPage;
 module.exports.signup = signup;
+module.exports.getToken = getToken;
--- server/router.js
+++ server/router.js
@@ -2,9 +2,10 @@ const controllers = require('./controllers');
 const middleware = require('./middleware');
 
 const router = (app) => {
+  app.get('/getToken', middleware.requiresSecure, controllers.Account.getToken);
+  app.get('/getDomos', middleware.requiresLogin, controllers.Domo.getDomos);
   app.get('/login', middleware.requiresSecure, middleware.requiresLogout, controllers.Account.loginPage);
   app.post('/login', middleware.requiresSecure, middleware.requiresLogout, controllers.Account.login);
-  app.get('/signup', middleware.requiresSecure, middleware.requiresLogout, controllers.Account.signupPage);
   app.post('/signup', middleware.requiresSecure, middleware.requiresLogout, controllers.Account.signup);
   app.get('/logout', middleware.requiresLogin, controllers.Account.logout);
   app.get('/maker', middleware.requiresLogin, controllers.Domo.makerPage);
